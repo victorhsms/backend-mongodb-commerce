111912c8ed6221ac93fdc13d361d64b5e145587d
Victor Hugo <victorhsmsantos@gmail.com>
2022-08-15 13:55:20 -0300

    requisito 16

---
+++ challenges/desafio16.js
@@ -0,0 +1,3 @@
+db.produtos.updateOne({ nome: "Big Mac" }, { $currentDate: { ultimaModificacao: true } }); 
+
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: false, nome: true });
